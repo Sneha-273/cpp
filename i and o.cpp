da8b084803b1ebb44f42e29a42b28963ac83ae38
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-02 21:47:20 +0530

    Add files via upload

---
+++ i and o.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{    
+    int n1=0,n2=0;
+    std::cout<<"Enter two numbers="<<std::endl;
+    std::cin>>n1>>n2;
+    std::cout<<"Sum of "<< n1<<" and "<<n2<<" is "<< n1+n2<<std::endl;
+    return 0;
+}
