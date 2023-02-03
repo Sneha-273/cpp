f8da17bfcc58631065f3029093591c10b88e1806
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-03 18:54:28 +0530

    Add files via upload

---
+++ namespace.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{    
+    int n1,n2;
+    cout<<"Enter first number:";
+    cin>>n1;
+    cout<<"Enter second number:";
+    cin>>n2;
+    cout<<"----------------------"<<endl;
+    cout<<"multiplication of those numbers is: "<<n1*n2;
+    return 0;
+}
