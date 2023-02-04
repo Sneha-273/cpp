deca5357394f99913e670af4a30e655e3cef60c4
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-04 19:50:35 +0530

    Add files via upload

---
+++ print series.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{    
+    int a=1,num;
+    cout<<"Enter the number=";
+    cin>>num;
+    while(a<=num)
+    {
+    cout<<a<<" "<<endl;
+    a=a+1;
+    }
+    
+    return 0;
+}
