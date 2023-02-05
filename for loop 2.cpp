117f3ebed8470c9f3d9517f4521e0dc60820270d
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-05 19:56:21 +0530

    Add files via upload

---
+++ for loop 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,n;
+    cout<<"Enter the number=";
+    cin>>n;
+    for(a=1;a<=n;a++)
+    {
+    cout<<a<<" "<<endl;
+    }
+    return 0;
+}
