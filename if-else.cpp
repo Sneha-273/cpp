2e692a46f3087927466a2e42bc8e0cbdb820ac59
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-06 19:46:15 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter the number=";
+    cin>>num;
+    if(num > 0)
+    {
+        cout<<num<<" "<<"is positive number";
+
+    }
+    else
+    {
+        cout<<num<<" "<<"is negative number";
+    }
+    return 0;
+}
