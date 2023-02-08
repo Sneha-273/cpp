4a271ae78d41256ec86d561c6d497c94b5d17be2
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-08 21:15:00 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"int= "<<sizeof(int)<<endl;
+    cout<<"unsinged int= "<<sizeof(unsigned int)<<endl;
+    cout<<"singed int= "<<sizeof(signed int)<<endl;
+    cout<<"char= "<<sizeof(char)<<endl;
+    cout<<"unsigned char= "<<sizeof(unsigned char)<<endl;
+    cout<<"singed char= "<<sizeof(signed char)<<endl;
+     cout<<"float= "<<sizeof(float)<<endl;
+    return 0;
+}
