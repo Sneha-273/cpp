d76f844ddb6134b13a6d4cd3ee87b4aabbdfdf41
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-09 20:37:48 +0530

    Add files via upload

---
+++ types conversation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    bool a=27;
+    int b=a;
+    b=3.14;
+    unsigned char c=-1;
+    double d=b;
+    cout<<"a="<<a <<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<int(c)<<endl;
+    cout<<"d="<<d;
+    return 0;
+}
