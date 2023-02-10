22b3c3587e43bf841cd8c0f6786514673a0d4cc4
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-10 19:33:15 +0530

    Add files via upload

---
+++ 10.interger literal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;   
+    a=25;
+    b=031;
+    c=0x19;
+    cout<<"..interger literal.."<<endl;
+    cout<<a<<endl<<b<<endl<<c;
+    return 0;
+}
---
+++ 11.float literal .cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c, d;
+    cout<<".. float point literal.."<<endl;
+    a=3.14159;
+    b=3.14159E2;
+    c=3.14159e3;
+    d=3.14159E-1;
+    cout<<a<<endl<<b<<endl<<c<<endl<<d;
+    return 0;
+}
