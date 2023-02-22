17ecd457281827d6bcd821fface7dde0a41c9001
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-02-22 20:11:35 +0530

    Add files via upload

---
+++ 28.square.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,n;
+    cout<<"enter the rows=";
+    cin>>n;
+    for(a=1; a<=n; a++)
+    {
+        for(b=1; b<=n; b++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
