d5eb2ce0ad93bac0be556608e8dc579c70b27761
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-03-19 19:18:35 +0530

    Add files via upload

---
+++ 41.enum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum day {Sunday, Monday, Thusday, Wednesday,Thursday,Friday,Saturday};
+    cout<<"Sunday= "<<Sunday<<endl;
+    cout<<"Monday= "<<Monday<<endl;
+    cout<<"Thusday= "<<Thusday<<endl;
+    cout<<"Wednesday= "<<Wednesday<<endl;
+    cout<<"Thursday= "<<Thursday<<endl;
+    cout<<"Friday= "<<Friday<<endl;
+    cout<<"Saturday= "<<Saturday;
+    return 0;
+}
