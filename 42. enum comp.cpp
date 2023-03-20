e30dca4cca3e0f5e29a11cb6b26d9999185c0235
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-03-20 21:16:40 +0530

    Add files via upload

---
+++ 42. enum comp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum colour{black, white, orange, red, green};
+    colour C1=black;
+    if(C1==0)
+    cout<<"it's true";
+    else
+    cout<<"it's false";
+    
+    return 0;
+}
