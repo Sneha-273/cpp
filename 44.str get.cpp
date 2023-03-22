a03e6dc483e994a391ae3962e44eb59199f102fd
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-03-22 19:29:28 +0530

    Add files via upload

---
+++ 44.str get.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char n[10],m[10];
+    /*cout<<"enter name= "<<endl;
+    cin>>n;
+    cout<<"you entered= "<<n<<endl;*/
+    cout<<"enter name= "<<endl;
+    cin.get(m,10);
+    cout<<"you entered= "<<m;
+    return 0;
+}
